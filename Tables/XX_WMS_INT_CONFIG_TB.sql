e835b40f0a761ce817cc52ed9929c0f600d975a2
tamilselvan10 <111409280+tamilselvan10@users.noreply.github.com>
2022-09-20 16:33:53 +0530

    Add files via upload

---
+++ Tables/XX_WMS_INT_CONFIG_TB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE XX_WMS_INT_CONFIG_TB
+(
+ID                      NUMBER        ,
+APPLICATION_ID          NUMBER        ,
+NAME                    VARCHAR2(400) ,
+FILENAME                VARCHAR2(400) ,
+MIME_TYPE               VARCHAR2(255) ,
+CREATED_ON              DATE      ,
+CONFIG_FILE             CLOB     
+);
